45f728286d640cac9769779c0a1a2c81c38e5bd2
Mina Chuong <mina.chuong@gmail.com>
2020-12-07 11:23:45 -0800

    update with solutions using objects

--- code-challenges.js
+++ code-challenges.js
@@ -7,7 +7,22 @@ var secretCodeWord1 = "lackadaisical"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
+function encodeMessage(secret) {
+  const encryptionMap = {
+    'a': 4,
+    'e': 3,
+    'i': 1,
+    'o': 0
+  };
 
+  const encryptionKeys = Object.keys(encryptionMap);
+
+  return secret
+          .split('')
+          .map(letter => 
+            encryptionKeys.includes(letter) ? encryptionMap[letter] : letter)
+          .join('');
+}
 
 
 
@@ -16,7 +31,9 @@ var secretCodeWord2 = "gobbledygook"
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
 
-
+function wordsContainingA(listOfWords) {
+  return listOfWords.filter(word => word.toLowerCase().includes('a'));
+}
 
 
 
@@ -31,9 +48,29 @@ var hand3 = [5, 5, 5, 5, 4]
 var hand4 = [3, 5, 3, 5, 3]
 // Expected output: true
 
-
 // STRETCH:
 var hand5 = ["K", 5, "K", 5, "K"]
 // Expected output: true
 var hand6 = ["A", "Q", "A", 5, 3]
 // Expected output: false
+
+function isFullHouse(hand) {
+  let occurrenceOfCards = {};
+  // map the array to a hash { "rank": countOfOccurrence }
+  hand.forEach((card) => {
+    const rank = card.toString();
+    if (Object.keys(occurrenceOfCards).includes(rank)) {
+      occurrenceOfCards[rank]++;
+    } else {
+      occurrenceOfCards[rank] = 1;
+    }
+  });
+  // continue if there are only two key-pairs 
+  if (Object.keys(occurrenceOfCards).length != 2) {
+    return false;
+  }
+
+  // continue to check if there is only one key-pair with count of 3 and one key-pair with count of 2
+  return Object.values(occurrenceOfCards).includes(2) 
+    && Object.values(occurrenceOfCards).includes(3);
+}
--- ruby_challenges.rb
+++ ruby_challenges.rb
@@ -1,15 +1,58 @@
 # ASSESSMENT 5: Ruby Coding Practical Questions
 
 # --------------------1) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data about each bike object.
+# class Bike 
+#   attr_accessor :model, :wheels, :frame_size
 
+#   def initialize model, frame_size, wheels=2
+#     @model = model
+#     @wheels = wheels
+#     @frame_size = frame_size
+#   end
 
+#   def get_info
+#     "The #{@model} bike has #{@wheels} wheels and a #{@frame_size} frame."
+#   end
+# end
 
-
+# bike = Bike.new 'trek', '123d'
+# p bike.get_info
 
 # Expected output example: 'The Trek bike has 2 wheels and a 168cm frame.'
 
 
 # -------------------2) Add a bell to the bike class and create the ability to ring the bell when the method is called.
+
+class Bell 
+  def ring
+    p "RING!"
+  end
+end
+
+class Bike 
+  attr_accessor :model, :wheels, :frame_size, :bell
+
+  def initialize model, frame_size, wheels=2
+    @model = model
+    @wheels = wheels
+    @frame_size = frame_size
+  end
+
+  def get_info
+    "The #{@model} bike has #{@wheels} wheels and a #{@frame_size} frame."
+  end
+
+  def more_info
+    p get_info
+  end
+end
+
+bike = Bike.new 'Sham', '100s'
+p bike.get_info
+bell = Bell.new
+bike.bell = bell
+bike.bell.ring
+bike.more_info
 
 
 
